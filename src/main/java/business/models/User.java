b34d4193fab719168e53d715248e57fdea2bb5d5
Riza Nugraha <rnugraha@gmail.com>
2015-03-20 17:21:44 +0100

    More attributes for User

--- src/main/java/business/models/User.java
+++ src/main/java/business/models/User.java
@@ -24,6 +24,9 @@ public class User implements Serializable {
     private Long id;    
     @Column(unique = true)
     private String username;
+    private String firstname;
+    private String lastname;
+    private String email;
     private String password;
     private boolean active = true;
     private boolean deleted = false;
@@ -57,6 +60,34 @@ public class User implements Serializable {
     
     public String getUsername() {
         return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
     
     public String getPassword() {

bc09fa75d00a4116bd3952eed672cb3353971cf5
Riza Nugraha <rnugraha@gmail.com>
2015-03-23 20:12:27 +0100

    Retrieve lab list by restangular

--- src/main/resources/static/app/components/registration/RegistrationController.js
+++ src/main/resources/static/app/components/registration/RegistrationController.js
@@ -1,20 +1,66 @@
 'use strict';
 
 angular.module('ProcessApp.controllers')
-    .controller('RegistrationController',['$scope', 'User', function ($scope, User) {
+    .controller('RegistrationController',['$scope', 'User', 'Lab', 'Restangular', function ($scope, User, Lab, Restangular) {
+
+        $scope.labs = []; // init labs
+
+        // Get lab list
+        Restangular.all('public/labs').getList()
+            .then(function (labs) {
+                $scope.labs = labs;
+            });
+
+        // -*-*-*-*-*-*-*-*-*-*-*- //
+        // START DUMMY  //
+        // -*-*-*-*-*-*-*-*-*-*-*- //
+        $scope.user = {
+            firstName :'Foo',
+            lastName : 'Bar',
+            email: 'foo@dntp.nl',
+            telephone: '090000000',
+            institution : '15',
+            lab : '12',
+            specialism : 'Phd Candidate',
+            isPathologyLabMember : true,
+            password : '12345678',
+            repeatPassword : '12345678'
+        };
+        // -*-*-*-*-*-*-*-*-*-*-*- //
+        // END TOO LAZY TO TYPE    //
+        // -*-*-*-*-*-*-*-*-*-*-*- //
+
         $scope.submit = function () {
             if ($scope.registrationForm.$valid) {
-                var user = new User(
-                    $scope.registrationForm.email,
-                    $scope.registrationForm.password,
-                    false,
-                    null
-                );
-                user.$register().then(function () {
-                    console.log("register success");
-                }, function (err) {
-                    console.log("register error");
+                var user;
+
+                Restangular.one('public/labs', $scope.user.lab).get()
+                    .then (function (lab) {
+
+                    console.log("selected lab is .. ", lab);
+
+                    user = new User({
+                        email:  $scope.user.email,
+                        firstname: $scope.user.firstName,
+                        lastname: $scope.user.lastName,
+                        telephone: $scope.user.telephone,
+                        institution: $scope.user.institution,
+                        lab: lab,
+                        specialism: $scope.user.specialism,
+                        isPathologyLabMember: $scope.user.isPathologyLabMember,
+                        password: $scope.user.password,
+                        null:null
+                    });
+
+                    // TODO register new user with restangular
+                    //user.$register(function(result) {
+                    //    console.log("success ... ", result);
+                    //});
                 });
+
+
+
+
             }
         };
 }]);

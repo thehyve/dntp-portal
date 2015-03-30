fd6fca01fbc5bc2221104f44add0ff7d336b630a
Gijs Kant <gijs@thehyve.nl>
2015-03-30 15:37:01 +0200

    Fixed roles and navigation.

--- src/main/resources/static/app/components/navbar/NavigationController.js
+++ src/main/resources/static/app/components/navbar/NavigationController.js
@@ -4,6 +4,40 @@ angular.module('ProcessApp.controllers')
     .controller('NavigationController',['$rootScope', '$scope', '$http', '$location', '$route', '$translate', '$cookieStore',
         function ($rootScope, $scope, $http, $location, $route, $translate, $cookieStore) {
 
+            var authenticate = function(callback) {
+                $http.get('user').success(function(data) {
+                    if (data.name) {
+    
+                        $rootScope.username = data.name;
+                        $rootScope.authenticated = true;
+                        $rootScope.roles = [];
+    
+                        $rootScope.globals = {
+                            currentUser: {
+                                username: $rootScope.username,
+                                credentials: $scope.credentials
+                            }
+                        };
+    
+                        $cookieStore.put('globals', $rootScope.globals);
+    
+                        if (data.authorities) {
+                            for(var i in data.authorities) {
+                                $rootScope.roles.push(data.authorities[i].authority);
+                            }
+                        }
+                        console.log("User '" +  data.name + "' has roles: " + JSON.stringify($rootScope.roles, null, 2));
+                    } else {
+                        $rootScope.authenticated = false;
+                    }
+                    callback && callback();
+                }).error(function() {
+                    $rootScope.authenticated = false;
+                    callback && callback();
+                });
+            };
+            authenticate();
+        
             $scope.logout = function() {
                 $http.post('logout', {}).success(function() {
                     $location.path("/login");

839b8368dec530cbde5d85f2df1ec4815328a159
Gijs Kant <gijs@thehyve.nl>
2015-04-14 12:48:25 +0200

    Added activation and deactivation of labs and backend for updating lab info by lab users.

--- src/main/java/business/models/LabRepository.java
+++ src/main/java/business/models/LabRepository.java
@@ -1,9 +1,14 @@
 package business.models;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LabRepository extends JpaRepository<Lab, Long> {
 
     Lab findByName(String lab);
-
+    
+    List<Lab> findAllByActiveTrue();
+    
+    Lab findOneByActiveTrue(Long id);
 }

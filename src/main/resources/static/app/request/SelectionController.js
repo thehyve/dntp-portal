ab4caa43f86732eae30a0c81c36b95269133c8ea
Gijs Kant <gijs@thehyve.nl>
2015-05-20 09:56:17 +0200

    Use key events for selecting excerpts.

--- src/main/resources/static/app/request/SelectionController.js
+++ src/main/resources/static/app/request/SelectionController.js
@@ -20,13 +20,30 @@ angular.module('ProcessApp.controllers')
                 });
             };
             
+            $scope.currentIndex = 0;
+            
+            $scope.relevantFields = ["Geslacht_Afkorting", "GebDat"]
+            $scope.relevantIndexes = []
+            if ($scope.request) {
+                for (var field in $scope.relevantFields) {
+                    var index = $scope.request.excerptList.columnNames.indexOf($scope.relevantFields[field]);
+                    console.log("field "+field+": index = "+index);
+                    $scope.relevantIndexes.push(index);
+                }
+            }
+            
+            $scope.toggleExcerpt = function(request, excerpt) {
+                console.log("Toggle excerpt: " + excerpt.id + " for request " + request.processInstanceId);
+                $scope.updateSelection(request, excerpt, !excerpt.selected);
+            }
+            
             $scope.selectExcerpt = function(request, excerpt) {
                 console.log("Select excerpt: " + excerpt.id + " for request " + request.processInstanceId);
                 $scope.updateSelection(request, excerpt, true);
             }
             
             $scope.deselectExcerpt = function(request, excerpt) {
-                console.log("Select excerpt: " + excerpt.id + " for request " + request.processInstanceId);
+                console.log("Deselect excerpt: " + excerpt.id + " for request " + request.processInstanceId);
                 $scope.updateSelection(request, excerpt, false);
             }
             
@@ -46,5 +63,50 @@ angular.module('ProcessApp.controllers')
                     }
                 });
             }
+            
+            $scope.scrollToCurrent = function() {
+                var elementId = '#excerpt_'+$scope.request.excerptList.entries[$scope.currentIndex].id;
+                if ($(elementId + ":not(:visible)")) {
+                    console.log("scroll to: " + elementId);
+                    $('html, body').animate({
+                        scrollTop: $(elementId).offset().top - 100
+                    }, 200);
+                }
+            }
+            
+            $(document).keydown(function(e){
+                if(e.which == 40) { // down
+                    console.log("down: " + $scope.request.excerptList.entries.length);
+                    if ($scope.currentIndex < $scope.request.excerptList.entries.length - 1) {
+                        $scope.$apply(function() {
+                            $scope.currentIndex++;
+                        });
+                        $('#excerpt_'+$scope.request.excerptList.entries[$scope.currentIndex].id).focus();
+                    }
+                    $scope.scrollToCurrent();
+                    return false; // stops the page from scrolling
+                }
+                if(e.which == 38) { // up
+                    console.log("up");
+                    if ($scope.currentIndex > 0) {
+                        $scope.$apply(function() {
+                            $scope.currentIndex--;
+                        });
+                    }
+                    $scope.scrollToCurrent();
+                    return false; // stops the page from scrolling
+                }
+                if(e.which == 32) { // space
+                    console.log("toggle");
+                    $scope.$apply(function() {
+                        $scope.toggleExcerpt($scope.request, $scope.request.excerptList.entries[$scope.currentIndex]);
+                        /*if ($scope.currentIndex < $scope.request.excerptList.entries.length - 1) {
+                            $scope.currentIndex++;
+                        }*/
+                    });
+                    $scope.scrollToCurrent();
+                    return false; // stops the page from scrolling
+                }
+            });
 
 }]);

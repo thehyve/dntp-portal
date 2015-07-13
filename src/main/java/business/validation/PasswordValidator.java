5b8235c13cb2fafc3ca9ad36acbee98d973ef3cd
Riza Nugraha <rnugraha@gmail.com>
2015-07-13 15:50:18 +0200

    Refactoring and unit testing

--- src/main/java/business/validation/PasswordValidator.java
+++ src/main/java/business/validation/PasswordValidator.java
@@ -5,16 +5,52 @@ import java.util.regex.Pattern;
 
 public class PasswordValidator {
 
-    public static boolean validate(String password) {
-
-        // =====================================================================
-        // REGEX (?=.*[!*'();:@&=+$,/?#[\]])+[a-zA-Z0-9!*'();:@&=+$,/?#[\]]{8,}
-        // at least one or more special chars and alphanumeric min 8 chars length
-        // =====================================================================
-
-        String regex = "\\(?=.*\\[!*'\\(\\);:@&=+$,/?#\\[\\\\]\\]\\)+\\[a-zA-Z0-9!*'\\(\\);:@&=+$,/?#\\[\\\\]\\]\\{8,\\}";
+    /**
+     * Create Matcher
+     *
+     * @param regex
+     * @param password
+     * @return
+     */
+    private static Matcher createMatcher(String regex, String password) {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
-        return matcher.find();
+        return matcher;
+    }
+
+    /**
+     * Password should contains following characters
+     * - alphabets in lower or capital letters
+     * - numerical chars
+     * - special chars ?=!*'();:@&=+$,/?#
+     *
+     * @param password
+     * @return true if valid, false if not valid
+     */
+    public static boolean validate(String password) {
+
+        boolean validPassword = false;
+
+        if (password != null) {
+
+            String numericalRegex = "(?=.*[0-9])";
+            String alphabeticalRegex = "(?=.*[a-z])";
+            String specialCharsRegex = "(?=.*[@#$%^&+=])";
+            String atLeastEightCharsRegex = ".{8,}";
+
+            Matcher numericalMatcher = createMatcher(numericalRegex, password);
+            Matcher alphabeticalMatcher = createMatcher(alphabeticalRegex, password);
+            Matcher specialCharsMatcher = createMatcher(specialCharsRegex, password);
+            Matcher minLengthMatcher = createMatcher(atLeastEightCharsRegex, password);
+
+            if (minLengthMatcher.find()) {
+                if (specialCharsMatcher.find()) {
+                    if (alphabeticalMatcher.find() || numericalMatcher.find()) {
+                        validPassword = true;
+                    }
+                }
+            }
+        }
+        return validPassword;
     }
 }
---
+++ src/test/java/business/validation/PasswordValidatorTests.java
@@ -0,0 +1,87 @@
+package business.validation;
+
+import org.junit.Test;
+import static  org.junit.Assert.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PasswordValidatorTests {
+
+    @Autowired
+    PasswordValidator passwordValidator;
+
+    @Test
+    public void testNullPasswordValidation() throws Exception {
+        assertFalse(PasswordValidator.validate(null));
+    }
+
+    @Test
+    public void testEmptyStringPasswordValidation() throws Exception {
+        assertFalse(PasswordValidator.validate(""));
+    }
+
+    @Test
+    public void testOneSpacePasswordValidation() throws Exception {
+        assertFalse(PasswordValidator.validate(" "));
+    }
+
+    @Test
+    public void testSpacePasswordValidation() throws Exception {
+        assertFalse(PasswordValidator.validate("                  "));
+    }
+
+    @Test
+    public void testWrongLengthValidation() throws Exception {
+        assertFalse(PasswordValidator.validate("3@sdfk"));
+    }
+
+    @Test
+    public void testAllCharsValidation() throws Exception {
+        assertFalse(PasswordValidator.validate("asdfghjk"));
+    }
+
+    @Test
+    public void testAllNumbersValidation() throws Exception {
+        assertFalse(PasswordValidator.validate("99999999"));
+    }
+
+    @Test
+    public void testNoSpecialCharsValidation() throws Exception {
+        assertFalse(PasswordValidator.validate("9999abcd"));
+    }
+
+    @Test
+    public void testNoCharsValidation() throws Exception {
+        assertTrue(PasswordValidator.validate("9999!@#$%"));
+    }
+
+    @Test
+    public void testNoNumbersValidation() throws Exception {
+        assertTrue(PasswordValidator.validate("abcd!@#$%"));
+    }
+
+    @Test
+    public void testValidPasswordValidation1() throws Exception {
+        assertTrue(PasswordValidator.validate("213abc!@"));
+    }
+
+    @Test
+    public void testValidPasswordValidation2() throws Exception {
+        assertTrue(PasswordValidator.validate("abc!@213"));
+    }
+
+    @Test
+    public void testValidPasswordValidation3() throws Exception {
+        assertTrue(PasswordValidator.validate("!@213abc"));
+    }
+
+    @Test
+    public void testValidPasswordValidation4() throws Exception {
+        assertTrue(PasswordValidator.validate("abc123&*"));
+    }
+
+    @Test
+    public void testValidPasswordWithSpaceValidation() throws Exception {
+        assertTrue(PasswordValidator.validate("abc 123 !@#"));
+    }
+
+}

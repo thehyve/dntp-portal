17f56da988e5a7ba837b3e3339b245225cf5b722
Bernd van der Veen <bernd_vanderveen@dfci.harvard.edu>
2016-06-01 15:44:49 +0200

    Add reopen notification email button incl translation

--- src/main/resources/static/messages/messages_nl.js
+++ src/main/resources/static/messages/messages_nl.js
@@ -178,6 +178,7 @@
         'popover: Upload medical ethical committee approval': 'Upload brief van medisch-ethische toetsingscommissie.',
         'Close request': 'Aanvraag afsluiten',
         'Compose mail to members': 'Schrijf e-mail aan leden',
+        'Compose reopen email': 'Meld heropenen aanvraag',
 
         'ppc_handled_according_mandate': 'Afgehandeld conform mandaat PPC',
         'ppc_handled_according_mandate_explanation': 'Afgehandeld conform mandaat PPC, zie toelichting',

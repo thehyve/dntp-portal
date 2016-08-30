bf0f69ceea71f842033863ed7c8aa00a4c5fce79
Gijs Kant <gijs@thehyve.nl>
2016-08-30 11:06:45 +0200

    Change cache control of static assets to include 'must-validate'.

--- src/main/java/business/security/CacheControlConfiguration.java
+++ src/main/java/business/security/CacheControlConfiguration.java
@@ -5,19 +5,25 @@
  */
 package business.security;
 
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class CacheControlConfiguration extends WebMvcConfigurerAdapter {
 
+    static final CacheControl cacheControl = CacheControl.maxAge(3600, TimeUnit.SECONDS).mustRevalidate();
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Add caching for static resources (1 hour).
         registry.addResourceHandler("/**")
             .addResourceLocations("classpath:/static/")
-            .setCachePeriod(3600);
+            .setCachePeriod(3600)
+            .setCacheControl(cacheControl);
     }
 
 }

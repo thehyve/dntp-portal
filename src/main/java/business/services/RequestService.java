09acbbb86d4ff67f0791cee46b6564daedb23e87
Gijs Kant <gijs@thehyve.nl>
2016-09-28 14:58:50 +0200

    Add lab name to request export.

--- src/main/java/business/services/RequestService.java
+++ src/main/java/business/services/RequestService.java
@@ -511,7 +511,10 @@ public class RequestService {
                 values.add(booleanToString(request.isMaterialsRequest()));
                 values.add(booleanToString(request.isClinicalDataRequest()));
                 values.add(request.getRequesterName());
-                values.add(request.getLab() == null ? "" : request.getLab().getNumber().toString());
+                values.add(request.getLab() == null ?
+                        "" :
+                        request.getLab().getNumber().toString() + ". " + request.getLab().getName()
+                        );
                 values.add(request.getRequester() == null ? "" : request.getRequester().getSpecialism());
                 values.add(labRequestService.countHubAssistanceLabRequestsForRequest(
                         request.getProcessInstanceId()).toString());

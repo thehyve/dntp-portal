16cec79fa8c69234eaaa76ccdc32a53814c4a746
barteld klasens <barteld@thehyve.nl>
2017-02-28 14:37:03 +0100

    step to click Edit or Deactivate on a lab

--- e2e/util.js
+++ e2e/util.js
@@ -12,25 +12,25 @@ chai.use(chaiAsPromised);
 var rp = require('request-promise');
 
 function newMapping(from, to, action) {
-    return { 'from': from, 'to': to, 'action': action };
+    return {'from': from, 'to': to, 'action': action};
 }
 
 var mappings = [
-    newMapping('login', 'forgot password', function() {
+    newMapping('login', 'forgot password', function () {
         var pages = require('./pages/pages');
         return pages.login.forgotPasswordButton.click();
     }),
-    newMapping('requests', 'create new request', function() {
+    newMapping('requests', 'create new request', function () {
         var pages = require('./pages/pages');
         return pages.requests.createNewRequest.click();
     }),
-    newMapping('*', 'lab requests', function() {
+    newMapping('*', 'lab requests', function () {
         return element(by.id('nav_lab-requests')).click();
     }),
-    newMapping('*', 'requests', function() {
+    newMapping('*', 'requests', function () {
         return element(by.id('nav_requests')).click();
     }),
-    newMapping('*', 'samples', function() {
+    newMapping('*', 'samples', function () {
         return element(by.id('nav_samples')).click();
     })
 ];
@@ -42,22 +42,22 @@ module.exports = {
         'forgot password': 'login/forgot-password',
         'requests': '',
         'lab requests': 'lab-requests',
-        'samples': 'samples'
-		'labs': 'labs'
+        'samples': 'samples',
+        'labs': 'labs'
     },
     users: {
-        'palga': { username: 'test+palga@dntp.thehyve.nl', password: 'palga' },
-        'requester': { username: 'test+requester@dntp.thehyve.nl', password: 'requester' },
-        'pathologist': { username: 'test+pathologist@dntp.thehyve.nl', password: 'requester' },
-        'principal_investigator': { username: 'test+contactperson@dntp.thehyve.nl', password: 'requester' },
-        'scientific council': { username: 'test+scientific_council@dntp.thehyve.nl', password: 'scientific_council' },
-        'lab': { username: 'test+lab_user106@dntp.thehyve.nl', password: 'lab_user' },
-        'hub': { username: 'test+hub_user@dntp.thehyve.nl', password: 'hub_user' },
-        'invalid': { username: 'kjaurtyqkuwygf', password: '784yfsda' }
+        'palga': {username: 'test+palga@dntp.thehyve.nl', password: 'palga'},
+        'requester': {username: 'test+requester@dntp.thehyve.nl', password: 'requester'},
+        'pathologist': {username: 'test+pathologist@dntp.thehyve.nl', password: 'requester'},
+        'principal_investigator': {username: 'test+contactperson@dntp.thehyve.nl', password: 'requester'},
+        'scientific council': {username: 'test+scientific_council@dntp.thehyve.nl', password: 'scientific_council'},
+        'lab': {username: 'test+lab_user106@dntp.thehyve.nl', password: 'lab_user'},
+        'hub': {username: 'test+hub_user@dntp.thehyve.nl', password: 'hub_user'},
+        'invalid': {username: 'kjaurtyqkuwygf', password: '784yfsda'}
     },
     'mappings': mappings,
     'chai': chai,
-    getPage: function(page) {
+    getPage: function (page) {
         var relativeUrl = this.relativeUrls[page];
         if (relativeUrl !== undefined) {
             return browser.get(this.baseUrl + this.relativeUrls[page]);
@@ -65,14 +65,14 @@ module.exports = {
             this.fatalError('The page ' + page + ' doesn\'t exist');
         }
     },
-    login: function(user) {
+    login: function (user) {
         var userObj;
 
         // Special case for lab users
         var matches = user.match(/^lab (\d+)$/);
         if (matches !== null) {
             var labId = matches[1];
-            userObj = { username: 'test+lab_user' + labId + '@dntp.thehyve.nl', password: 'lab_user' };
+            userObj = {username: 'test+lab_user' + labId + '@dntp.thehyve.nl', password: 'lab_user'};
         } else {
             userObj = this.users[user];
         }
@@ -87,25 +87,26 @@ module.exports = {
             this.fatalError('The user ' + user + ' doesn\'t exist');
         }
     },
-    logout: function() {
+    logout: function () {
         var rawBaseUrl = 'http://localhost:8092/';
 
         // We test if we are logged in by trying to GET requests
         return rp({uri: rawBaseUrl + 'requests', resolveWithFullResponse: true})
-            .then(function(response) {
+            .then(function (response) {
                 // Click on the logout button of the navbar to logout!
                 var pages = require('./pages/pages');
                 return pages.nav.logout();
             })
-            .catch(function() {}); // This will catch a possible 403 error;
+            .catch(function () {
+            }); // This will catch a possible 403 error;
     },
-    clearRequests: function() {
+    clearRequests: function () {
         // Get a page to have the CSRF token
-        return this.getPage('login').then(function() {
+        return this.getPage('login').then(function () {
             return browser.driver.get('http://localhost:8092/test/clear');
         });
     },
-    fatalError: function(message) {
+    fatalError: function (message) {
         throw 'Fatal error: the specification is incorrectly expressed! ' + message;
     }
 }

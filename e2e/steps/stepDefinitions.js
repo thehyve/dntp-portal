f238e59541c2c37b43f9dda320874e4a6fc50975
barteld klasens <barteld@thehyve.nl>
2017-04-05 12:27:54 +0200

    fix for DNTP-567 e2e step to check billing address on pakbon does not work

--- e2e/steps/stepDefinitions.js
+++ e2e/steps/stepDefinitions.js
@@ -310,7 +310,6 @@ module.exports = function () {
             var newWindowHandle = handles[1]; // this is your new window
             browser.switchTo().window(newWindowHandle).then(function () {
                 browser.sleep(5000).then(function () {
-					browser.actions().sendKeys(protractor.Key.ESCAPE).perform();
                     browser.actions().sendKeys(protractor.Key.ESCAPE).perform().then(function () {
                         next();
                     });
@@ -421,6 +420,32 @@ module.exports = function () {
                         next()
                     }
                 }, next);
+            });
+        });
+    });
+
+    this.Then(/^the static form in a new tab contains\w*$/, function (fields, next) {
+        var regex = /^(.+): (.+)\s*$/;
+
+        var lines = fields.split('\n');
+
+        browser.getAllWindowHandles().then(function (handles) {
+            var newWindowHandle = handles[1]; // this is your new window
+            browser.switchTo().window(newWindowHandle).then(function () {
+                lines.forEach(function (line) {
+                    var matches = line.match(regex);
+                    var label = matches[1];
+                    var content = matches[2];
+
+                    Promise.resolve(element(by.cssContainingText('label.ng-binding', label)).element(by.xpath('..')).$$('.ng-binding')).then(function (el) {
+                        expect(el[1].getText()).to.eventually.contain(content).then(function (value) {
+                            console.log(value);
+                            if (value.includes(lines[lines.length - 1].match(regex)[2])) { //hack to wait for last check to be done.
+                                next()
+                            }
+                        }, next);
+                    });
+                });
             });
         });
     });
